589bdefbbb2ed09898b58c419b5b37349a30bd78
sh2MAN <simons2007@yandex.ru>
2020-12-16 00:43:48 +0400

    Fix api_yamdb/urls.py (flake8)

--- api_yamdb/urls.py
+++ api_yamdb/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
-from rest_framework_simplejwt.views import (TokenObtainPairView,
-                                            TokenRefreshView)
+# from rest_framework_simplejwt.views import (TokenObtainPairView,
+# TokenRefreshView)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
