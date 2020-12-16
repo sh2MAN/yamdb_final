cc005185c62b93c33394b880421c0fe02310c91c
sh2MAN <simons2007@yandex.ru>
2020-12-17 00:01:12 +0400

    Final deploy

--- api_yamdb/urls.py
+++ api_yamdb/urls.py
@@ -16,8 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
-# from rest_framework_simplejwt.views import (TokenObtainPairView,
-# TokenRefreshView)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,14 +26,4 @@ urlpatterns = [
         TemplateView.as_view(template_name='redoc.html'),
         name='redoc'
     ),
-    # path(
-    #     'v1/auth/token/',
-    #     TokenObtainPairView.as_view(),
-    #     name='token_obtain_pair'
-    # ),
-    # path(
-    #     'v1/auth/token/refresh/',
-    #     TokenRefreshView.as_view(),
-    #     name='token_refresh'
-    # ),
 ]
